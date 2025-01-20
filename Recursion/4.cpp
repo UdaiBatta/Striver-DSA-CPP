c8a0078b25ce1bd075a53bb5248dd3d943ccbc96
Kr4ter <udbatta@gmail.com>
2025-01-21 00:01:10 +0530

    Recursion Revision started

---
+++ Recursion/4.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void func(int i , int sum){
+    if(i<1){
+        cout<<sum<<endl;
+        return;
+    }
+     func(i-1,sum+i);
+}
+int main(){
+    int n= 3;
+    func(n,0);
+    return 0;
+}
